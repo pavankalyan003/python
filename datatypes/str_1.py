6497ea415ee199131ece20763af4c058afc52367
pavankalyan003 <maigapulapavankalyan@gmail.com>
2026-08-03 12:31:54 +0530

    Add files via upload

---
+++ datatypes/str_1.py
@@ -0,0 +1,8 @@
+first = "pavan"
+last = "kalyan"
+full_name = first + " " + last
+print(full_name.upper())
+print(full_name.lower())
+print(full_name.title())
+print(len(full_name))
+print(full_name[0], full_name[-1])
---
+++ datatypes/str_2.py
@@ -0,0 +1,8 @@
+first = "pavan"
+last = "kalyan"
+full_name = first + " " + last
+
+# Extract and print only the first name using string slicing on full_name
+first_name_extracted = full_name[0:5]
+
+print("Extracted First Name:", first_name_extracted)
