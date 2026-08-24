406f2f68fcabb4b0d879892b627ac5bf3e9f067f
pavankalyan003 <maigapulapavankalyan@gmail.com>
2026-08-24 13:00:56 +0530

    Add files via upload

---
+++ lists/list10.py
@@ -0,0 +1,9 @@
+#Write a program that uses a negative step slice to print a list in reverse order.
+num=[1,2,3,4,5]
+print("reverse order:",num[::-1])
+
+
+#output:
+#reverse order: [5, 4, 3, 2, 1]
+
+
---
+++ lists/list2.py
@@ -0,0 +1,12 @@
+list1 = [10, 3.14, "Hello", True, [1, 2, 3]]
+
+for element in list1:
+    print(element, "->", type(element))
+
+
+#output:
+#10 -> <class 'int'>
+#3.14 -> <class 'float'>
+#Hello -> <class 'str'>
+#True -> <class 'bool'>
+#[1, 2, 3] -> <class 'list'>
